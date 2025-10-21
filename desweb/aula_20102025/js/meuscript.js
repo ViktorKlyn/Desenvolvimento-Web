98ec8f4beb27047401cdab46e17b8bdf184e0fa2
JOÃO VICTOR KLEIN <144391590+ViktorKlyn@users.noreply.github.com>
2025-10-20 21:43:37 -0300

    Add files via upload

---
+++ desweb/aula_20102025/js/meuscript.js
@@ -0,0 +1,16 @@
+function exibir(){
+    document.getElementById('texto').innerHTML = 'Seu mouse está sobre o texto';
+} 
+
+function ocultar(){
+    document.getElementById('texto').innerHTML = '';
+}
+
+// Crie uma nova sessão no HTML
+/*
+    Desenvolva um botão que ao receber um clique mude a cor de um ou mais elementos
+ */
+
+    function mudarcortexto(){
+        
+    }
